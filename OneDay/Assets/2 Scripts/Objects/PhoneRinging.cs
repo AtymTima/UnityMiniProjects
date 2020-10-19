111372c7849117fbafa5f3f366dffb0ce168110f
AtymTima <timanis188@gmail.com>
2020-10-19 23:51:56 +0600

    Added new levels in Hong Kong

---
+++ OneDay/Assets/2 Scripts/Objects/PhoneRinging.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+
+public class PhoneRinging : MonoBehaviour
+{
+    [SerializeField] private Transform cameraTransform;
+    [SerializeField] private Transform backgroundTransform;
+    [SerializeField] private AudioSource phoneSource;
+    [SerializeField] private const float defaultShakeDuration = 0.2f;
+    [SerializeField] private const float defaultShakePower = 0.25f;
+    [SerializeField] private const float defaultShakeSpeed = 10f;
+    public bool IsShaking { get; private set; }
+    private float currentDuration;
+    private float currentPower;
+    private float currentSpeed;
+    private Vector3 initialTransformCamera;
+    private Vector3 initialTransformBackground;
+    private Vector3 nextPosition;
+
+    private void LateUpdate()
+    {
+        switch (IsShaking)
+        {
+            case true:
+                if (currentDuration > 0)
+                {
+                    ChangeCameraPosition();
+                    currentDuration -= Time.deltaTime;
+                }
+                else
+                {
+                    StopShaking();
+                }
+                break;
+        }
+    }
+
+    private void ChangeCameraPosition()
+    {
+        cameraTransform.localPosition = Vector3.MoveTowards(cameraTransform.localPosition, nextPosition, currentSpeed * Time.deltaTime);
+        backgroundTransform.localPosition = Vector3.MoveTowards(backgroundTransform.localPosition, nextPosition, currentSpeed * Time.deltaTime);
+
+        if (cameraTransform.localPosition == nextPosition)
+        {
+            nextPosition.x = initialTransformCamera.x + Random.Range(-currentPower, currentPower);
+            nextPosition.y = initialTransformCamera.y + Random.Range(-currentPower, currentPower);
+        }
+    }
+
+    private void StopShaking()
+    {
+        IsShaking = false;
+        cameraTransform.localPosition = initialTransformCamera;
+        backgroundTransform.localPosition = initialTransformBackground;
+    }
+
+    public void ShakeSurroundings(int isFirstVibration)
+    {
+        switch(isFirstVibration)
+        {
+            case 1:
+                phoneSource.Play();
+                break;
+        }
+        IsShaking = true;
+        currentDuration = defaultShakeDuration;
+        currentPower = defaultShakePower;
+        currentSpeed = defaultShakeSpeed;
+        initialTransformCamera = cameraTransform.localPosition;
+        initialTransformBackground = backgroundTransform.localPosition;
+        nextPosition = initialTransformCamera;
+    }
+}
--- OneDay/Assets/2 Scripts/Platforms/PlatformMoving.cs
+++ OneDay/Assets/2 Scripts/Platforms/PlatformMoving.cs
@@ -1,12 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlatformMoving : MonoBehaviour
 {
     [SerializeField] private Transform platformTransform;
     [SerializeField] private bool isHorizontal = true;
     [SerializeField] private float speedOfMoving = 10f;
+    [SerializeField] private SpriteRenderer mySprite;
+    [SerializeField] private bool isFlippable;
     private Vector3 currentDirection;
     private Vector3 rightDirection = new Vector2(1f, 0);
     private Vector3 upDirection = new Vector2(0, 1f);
@@ -28,6 +28,12 @@ public class PlatformMoving : MonoBehaviour
         switch (collision.CompareTag(platformTag))
         {
             case true:
+                switch(isFlippable)
+                {
+                    case true:
+                        mySprite.flipX = !mySprite.flipX;
+                        break;
+                }
                 currentDirection *= -1;
                 break;
         }
