f3bbd88cabdcecd15f205a552110c7c1f5aa9852
AtymTima <timanis188@gmail.com>
2020-10-24 00:24:43 +0600

    Added "Impostor" Character and changed to URP

---
+++ AmongUsHalloween/Assets/2 Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [Header("Cash References")]
+    [SerializeField] private Transform cameraTransform;
+    private Transform targetPlayer;
+    private Vector3 currentPos;
+
+    private void Awake()
+    {
+        targetPlayer = cameraTransform.parent.GetComponentInChildren<PlayerMovement>().transform;
+        currentPos.z = -1;
+    }
+
+    private void Update()
+    {
+        currentPos.x = targetPlayer.localPosition.x;
+        currentPos.y = targetPlayer.localPosition.y;
+        cameraTransform.localPosition = currentPos;
+    }
+}
---
+++ AmongUsHalloween/Assets/2 Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Cashed References")]
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private Rigidbody2D playerRB;
+    [SerializeField] private InputAction WASD;
+    [SerializeField] private Animator playerAnim;
+    private bool isRunning;
+    private bool hasChanged;
+    [Header("Movement Parameters")]
+    [SerializeField] private float movementSpeed = 5f;
+    private Vector2 movementVector;
+    private Vector2 zeroVector = new Vector2(0, 0);
+    private Vector2 flipScale = new Vector2(0,1);
+    private float movementXPos;
+
+    private void OnEnable() { WASD.Enable(); }
+    private void OnDisable() { WASD.Disable(); }
+
+    private void Update()
+    {
+        movementVector = WASD.ReadValue<Vector2>();
+        movementXPos = movementVector.x;
+        if (movementVector != zeroVector)
+        {
+            switch(!hasChanged)
+            {
+                case true:
+                    hasChanged = true;
+                    isRunning = true;
+                    playerAnim.SetBool("isRunning", isRunning);
+                    break;
+            }
+            flipScale.x = Mathf.Sign(movementXPos);
+            playerTransform.localScale = flipScale;
+        }
+        else
+        {
+            switch (hasChanged)
+            {
+                case true:
+                    hasChanged = false;
+                    isRunning = false;
+                    playerAnim.SetBool("isRunning", isRunning);
+                    break;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        switch(isRunning)
+        {
+            case true:
+                playerRB.velocity = movementVector * movementSpeed * Time.fixedDeltaTime;
+                break;
+            case false:
+                movementVector.x = 0;
+                movementVector.y = 0;
+                playerRB.velocity = movementVector;
+                break;
+        }
+    }
+}
