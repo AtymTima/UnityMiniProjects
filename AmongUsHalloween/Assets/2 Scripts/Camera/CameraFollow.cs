05109eca2d9cd08db38348404863eb72a43f221a
AtymTima <timanis188@gmail.com>
2020-10-24 23:05:59 +0600

    Finished making the map

--- AmongUsHalloween/Assets/2 Scripts/Camera/CameraFollow.cs
+++ AmongUsHalloween/Assets/2 Scripts/Camera/CameraFollow.cs
@@ -4,19 +4,23 @@ public class CameraFollow : MonoBehaviour
 {
     [Header("Cash References")]
     [SerializeField] private Transform cameraTransform;
+    [Header("Parameters")]
+    [SerializeField] private float moveSpeed = 5f;
     private Transform targetPlayer;
     private Vector3 currentPos;
+    private Vector3 velocity;
 
     private void Awake()
     {
         targetPlayer = cameraTransform.parent.GetComponentInChildren<PlayerMovement>().transform;
-        currentPos.z = -1;
+        currentPos.z = -1; 
     }
 
-    private void Update()
+    void Update()
     {
-        currentPos.x = targetPlayer.localPosition.x;
-        currentPos.y = targetPlayer.localPosition.y;
-        cameraTransform.localPosition = currentPos;
+        currentPos = new Vector3(targetPlayer.position.x, targetPlayer.position.y, transform.position.z);
+        velocity = (currentPos - cameraTransform.position) * 4f;
+        transform.position = Vector3.SmoothDamp(cameraTransform.position, currentPos, 
+        ref velocity, 1.0f, moveSpeed * Time.deltaTime);
     }
 }
